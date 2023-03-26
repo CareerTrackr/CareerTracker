6af9d9797042068ff847311325e862e11cac3da9
Matthew <matthew.xing@gmail.com>
2023-03-26 15:01:32 -0700

    cleaned up linting errors for clean base

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-
   return (
-    <div className="App">
-      Hello World!
+    <div>
+      Hello World
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
