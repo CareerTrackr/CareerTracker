c2e365e8124c68132fe2b260983e3dbb923345dd
Matthew <matthew.xing@gmail.com>
2023-03-28 13:40:43 -0700

    integrated MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react';
 import { HashRouter } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Button variant="contained">delete me<DeleteIcon></DeleteIcon></Button>
+      
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   )
 }
 
