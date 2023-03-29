4432cbcd11a611c0b4a80acf5cec36f6d9b93265
Matthew <matthew.xing@gmail.com>
2023-03-28 19:49:54 -0700

    added dark mode by default, proposal to include light/dark option in header to change darkMode state

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
 import { HashRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import { Sidebar } from "./components/Sidebar.js";
+import { DarkMode } from '../types.js';
 
-function App() {
+function App ({setDarkMode}: DarkMode) {
   const [showHomepage, setShowHomepage] = useState(false);
   const [showApplications, setShowApplications] = useState(false);
   const [showNotes, setShowNotes] = useState(false);
   const [showNotifications, setShowNotifications] = useState(false);
   const [showSearchFilter, setShowSearchFilter] = useState(false);
-  
 
   return (
+    //<Header setDarkMode={setDarkMode}/> include dark mode option in header 
     <Sidebar 
       showApplications={showApplications} setShowApplications ={setShowApplications}
       showNotes={showNotes} setShowNotes={setShowNotes}
@@ -21,9 +24,21 @@ function App() {
 }
 
 function WrappedApp() {
+  const [darkMode, setDarkMode] = useState('dark');
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+    },
+  });
+
   return (
     <HashRouter>
-      <App />
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline/>
+        <App setDarkMode={setDarkMode}/>
+      </ThemeProvider>
+      
     </HashRouter>
   )
 }
