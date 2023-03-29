b3652fb7749e49d0e54ec189a9672bd62298e801
Matthew <matthew.xing@gmail.com>
2023-03-28 19:05:28 -0700

    added page options as an interface in types.ts

---
+++ types.ts
@@ -0,0 +1,10 @@
+export interface PageOptions {
+  showApplications: boolean;
+  showNotes: boolean;
+  showNotifications: boolean;
+  showSearchFilter: boolean;
+  setShowApplications: React.Dispatch<React.SetStateAction<boolean>>;
+  setShowNotes: React.Dispatch<React.SetStateAction<boolean>>;
+  setShowNotifications: React.Dispatch<React.SetStateAction<boolean>>;
+  setShowSearchFilter: React.Dispatch<React.SetStateAction<boolean>>;
+}
