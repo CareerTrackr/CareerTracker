f928d0b09f9cd18c20e31e1293cd5bc9d27ab231
Matthew <matthew.xing@gmail.com>
2023-04-02 04:04:18 -0700

    now with inspirational quotes!

--- src/views/homepage.tsx
+++ src/views/homepage.tsx
@@ -9,6 +9,24 @@ import { showHomepageOptions } from "../../types";
 export const Homepage: React.FunctionComponent<showHomepageOptions> = ({setShowHomepage}) => {
   const [showLogin, setShowLogin] = useState(false);
 
+  const shortQuotes = [
+    "'The secret of getting ahead is getting started.' - Mark Twain",
+    "'The best time to plant a tree was 20 years ago. The second best time is now.' - Chinese Proverb",
+    "'It's hard to beat a person who never gives up.' - Babe Ruth",
+    "'I believe in you.' - Matthew Xing",
+    "'Happiness is not something ready made. It comes from your own actions.' - Dalai Lama XIV",
+    "'Whatever you are, be a good one.' - Abraham Lincoln",
+    "'Your passion is waiting for your courage to catch up.' - Isabelle Lafleche",
+    "'If you’re offered a seat on a rocket ship, don’t ask what seat! Just get on.' - Sheryl Sandberg",
+    "'How wonderful it is that nobody need wait a single moment before starting to improve the world.' - Anne Frank",
+    "'Great things are done by a series of small things brought together.' - Vincent Van Gogh",
+    "'You can waste your lives drawing lines. Or you can live your life crossing them.' - Shonda Rhimes",
+    "'If opportunity doesn’t knock, build a door.' - Kurt Cobain",
+    "'Life is like a chess move - make your next move your very best.' - Matt Matias",
+    "'Never stop doing your best just because someone doesn’t give you credit.' - Lyrikal",
+    "'We can do anything we want to if we stick to it long enough.' - Helen Keller"
+  ]
+
   useEffect(() => {
     setTimeout(() => {
       setShowLogin(true);
@@ -34,7 +52,6 @@ export const Homepage: React.FunctionComponent<showHomepageOptions> = ({setShowH
   }));
 
   function handleLogin() {
-    //temporarily set showing the homepage to be false
     setShowHomepage(false);
   }
 
@@ -56,11 +73,13 @@ export const Homepage: React.FunctionComponent<showHomepageOptions> = ({setShowH
         </Fade>
         {showLogin ? (
           <Fade in={true} timeout={2000}>
-            <ColorButton size="large" variant="outlined" startIcon={<LoginIcon/>} onClick={handleLogin}>
-              Login
+            <ColorButton size="large" variant="outlined" startIcon={<LoginIcon/>} sx={{ textTransform: 'none' }} onClick={handleLogin}>
+              <Typography variant="h6">
+                {shortQuotes[Math.floor(Math.random() * shortQuotes.length)]}
+              </Typography>
             </ColorButton>
           </Fade>
-        ) : <Fade in={false} ><ColorButton size="large">InvisPlaceholder</ColorButton></Fade>}
+        ) : <Fade in={false} ><ColorButton size="large"><Typography variant="h6">InvisPlaceholder</Typography></ColorButton></Fade>}
       </ThemeProvider>
     </Box>
   )
