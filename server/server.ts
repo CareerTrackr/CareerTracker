8ec45321f33ee5e15746ae2c42803ca4662e1765
Matthew <matthew.xing@gmail.com>
2023-04-02 03:13:07 -0700

    added functionality to create a columns/rows template for the data file when the fetch request is received and no data file existed previously

--- server/server.ts
+++ server/server.ts
@@ -15,8 +15,47 @@ app.get("/", async (req: Request, res: Response) => {
     const data = await fs.promises.readFile('applicationData.json', 'utf8');
     res.send(data);
   }
-  catch(err){
-    await fs.promises.writeFile('applicationData.json', JSON.stringify({}));
+  catch(noFile){
+    await fs.promises.writeFile('applicationData.json', JSON.stringify({
+      columns: [
+        { field: 'id', 
+          headerName: 'ID', 
+          width: 50
+        },
+        {
+          field: 'date', 
+          headerName: 'Date',
+          width: 90,
+          editable: true,
+        },
+        {
+          field: 'status', 
+          headerName: 'Status',
+          width: 110,
+          editable: true,
+        },
+        {
+          field: 'company', 
+          headerName: 'Company',
+          width: 110,
+          editable: true,
+        },
+        {
+          field: 'role', 
+          headerName: 'Role',
+          width: 110,
+          editable: true,
+        },
+        {
+          field: 'link', 
+          headerName: 'Link',
+          width: 200,
+          editable: true,
+          renderCellLink: true,
+        },
+      ],
+      rows: [],
+    }));
     const data = await fs.promises.readFile('applicationData.json')
     res.send(data);
   }
