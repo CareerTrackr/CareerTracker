9df76616275a869701df911a87535a8216c1010f
Matthew <matthew.xing@gmail.com>
2023-04-07 13:47:48 -0700

    debugging import/require issues

--- server/server.ts
+++ server/server.ts
@@ -1,22 +1,22 @@
 import express from 'express';
 import { Request, Response } from "express";
 import cors from 'cors';
-const itemController = require('./controllers/itemController');
+import itemcontroller from './controllers/itemcontroller.js';
 const PORT = 5174;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.patch("/", itemController.patchDatabase, async (req: Request, res: Response) => {
+app.patch("/", itemcontroller.getDatabase, async (req: Request, res: Response) => {
   return res.status(200).send('Patch successful.')
 })
 
-app.post("/", itemController.postDatabase, async (req: Request, res: Response) => {
+app.post("/", itemcontroller.postDatabase, async (req: Request, res: Response) => {
   return res.status(200).send('Post successful.');
 })
 
-app.get("/", itemController.getDatabase, async (req: Request, res: Response) => {
+app.get("/", itemcontroller.getDatabase, async (req: Request, res: Response) => {
   return res.status(200).send(res.locals.data);
 });
 
